c742ef107134bf6e5bc8f0b930108a60289e4564
Rafael Herculano <rafa.lj@hotmail.com>
2024-04-01 18:02:38 -0300

    fix middleware s3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,41 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  isProduction
+    ? {
+        name: "strapi::security",
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              "connect-src": ["'self'", "https:"],
+              "img-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "dl.airtable.com",
+                "my-strapi-bucket-77.s3.us-east-1.amazonaws.com",
+              ],
+              "media-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "dl.airtable.com",
+                "my-strapi-bucket-77.s3.us-east-1.amazonaws.com",
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      }
+    : "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
