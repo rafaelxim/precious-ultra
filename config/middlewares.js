de4665dbcd036649a512f217cb12675d4fdeb4d7
Rafael Herculano <rafa.lj@hotmail.com>
2024-04-05 08:20:39 -0300

    fix middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,36 +1,6 @@
-const isProduction = process.env.NODE_ENV === "production";
-
 module.exports = [
   "strapi::logger",
   "strapi::errors",
-  isProduction
-    ? {
-        name: "strapi::security",
-        config: {
-          contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-              "connect-src": ["'self'", "https:"],
-              "img-src": [
-                "'self'",
-                "data:",
-                "blob:",
-                "dl.airtable.com",
-                "my-strapi-bucket-77.s3.us-east-1.amazonaws.com",
-              ],
-              "media-src": [
-                "'self'",
-                "data:",
-                "blob:",
-                "dl.airtable.com",
-                "my-strapi-bucket-77.s3.us-east-1.amazonaws.com",
-              ],
-              upgradeInsecureRequests: null,
-            },
-          },
-        },
-      }
-    : "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
@@ -38,5 +8,4 @@ module.exports = [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  { resolve: "../src/middlewares/admin-redirect.js" },
 ];
